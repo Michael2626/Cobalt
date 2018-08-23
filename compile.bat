2e61f5a411d18ba1118e794a38100ca5ae4b6fe7
Atnode <theatnode@gmail.com>
2018-08-23 20:13:00 +0200

    Updating compile.bat

--- compile.bat
+++ compile.bat
@@ -1,12 +1,24 @@
 @ECHO OFF
 
-SET NAME=Corbin Davenport
-SET PUBLISHER=Cobalt
+:: check argument
+if "%1"=="" goto noarg
+if "%1"=="en" (
+set cobaltlang=en
+) else if "%1"=="fr" (
+set cobaltlang=fr
+) else (
+goto errorarg
+)
+
+
+SET NAME=Cobalt ISO
+SET PUBLISHER=Atnode
 SET TITLE=Cobalt Live CD
-SET FILE=cobalt.iso
+SET VER=1.3
 SET DIR=%~dp0
 
 CD %DIR%
+
 
 :: check for mkisofs
 
@@ -28,34 +40,98 @@ if exist 7ZA.EXE (
 )
 
 :: compile base.zip
-if exist CDROOT\COBALT\BASE.ZIP (
-    del CDROOT\COBALT\BASE.ZIP
+if exist CDROOT\COBALT\ZIP\BASE.ZIP (
+    del CDROOT\COBALT\ZIP\BASE.ZIP
     echo [ OK ] Deleting existing BASE.ZIP file.
 )
-cd packages\base
-..\..\7za a ..\..\CDROOT\COBALT\BASE.ZIP *
+cd PACKAGES\base
+..\..\7za a ..\..\CDROOT\COBALT\ZIP\BASE.ZIP *
 cd ..\..\
 
 :: compile desktop.zip
-if exist CDROOT\COBALT\DESKTOP.ZIP (
-    del CDROOT\COBALT\DESKTOP.ZIP
+if exist CDROOT\COBALT\ZIP\DESKTOP.ZIP (
+    del CDROOT\COBALT\ZIP\DESKTOP.ZIP
     echo [ OK ] Deleting existing DESKTOP.ZIP file.
 )
-cd packages\desktop
-..\..\7za a ..\..\CDROOT\COBALT\DESKTOP.ZIP *
+cd PACKAGES\desktop
+..\..\7za a ..\..\CDROOT\COBALT\ZIP\DESKTOP.ZIP *
 cd ..\..\
 
-:: compile vbox.zip
-if exist CDROOT\COBALT\VBOX.ZIP (
-    del CDROOT\COBALT\VBOX.ZIP
-    echo [ OK ] Deleting existing VBOX.ZIP file.
+if "%cobaltlang%"=="fr" (
+goto frbuild 
+) else if "%cobaltlang%"=="en" (
+goto enbuild
 )
-cd packages\virtualbox
-..\..\7za a ..\..\CDROOT\COBALT\VBOX.ZIP *
-cd ..\..\
+
+:frbuild
+SET FILE=Cobalt-%VER%-FR.iso
+:: compile fr.zip
+if exist CDROOT\COBALT\ZIP\FR.ZIP (
+    del CDROOT\COBALT\ZIP\FR.ZIP
+    echo [ OK ] Deleting existing FR.ZIP file.
+)
+cd LANG\FR\PACKAGES
+..\..\..\7za a ..\..\..\CDROOT\COBALT\ZIP\FR.ZIP *
+cd ..\..\..\
+
+if exist CDROOT\COBALT\SETUP.BAT (
+    del CDROOT\COBALT\SETUP.BAT
+    echo [ OK ] Deleting existing SETUP.BAT file.
+) else if exist CDROOT\COBALT\REPAIR.BAT (
+    del CDROOT\COBALT\REPAIR.BAT
+    echo [ OK ] Deleting existing REPAIR.BAT file.
+)
+
+cd LANG\FR\BAT
+copy *.BAT ..\..\..\CDROOT\COBALT\
+cd ..\..\..\
+
+if exist CDROOT\ISOLINUX\BTDSK.IMG (
+    del CDROOT\ISOLINUX\BTDSK.IMG
+    echo [ OK ] Deleting existing BTDSK.IMG file.
+)
+
+cd LANG\FR\ISOLINUX
+copy *.IMG ..\..\..\CDROOT\ISOLINUX\
+cd ..\..\..\
+goto files
+
+:enbuild
+SET FILE=Cobalt-%VER%-EN.iso
+:: compile en.zip
+if exist CDROOT\COBALT\ZIP\EN.ZIP (
+    del CDROOT\COBALT\ZIP\EN.ZIP
+    echo [ OK ] Deleting existing EN.ZIP file.
+)
+
+cd LANG\EN\PACKAGES
+..\..\..\7za a ..\..\..\CDROOT\COBALT\ZIP\EN.ZIP *
+cd ..\..\..\
+
+if exist CDROOT\COBALT\SETUP.BAT (
+    del CDROOT\COBALT\SETUP.BAT
+    echo [ OK ] Deleting existing SETUP.BAT file.
+) else if exist CDROOT\COBALT\REPAIR.BAT (
+    del CDROOT\COBALT\REPAIR.BAT
+    echo [ OK ] Deleting existing REPAIR.BAT file.
+)
+
+cd LANG\EN\BAT
+copy *.BAT ..\..\..\CDROOT\COBALT\
+cd ..\..\..\
+
+if exist CDROOT\ISOLINUX\BTDSK.IMG (
+    del CDROOT\ISOLINUX\BTDSK.IMG
+    echo [ OK ] Deleting existing BTDSK.IMG file.
+)
+
+cd LANG\EN\ISOLINUX
+copy *.IMG ..\..\..\CDROOT\ISOLINUX\
+cd ..\..\..\
+goto files
 
 :: check for files
-
+:files
 if exist %FILE% (
     del %FILE%
     echo [ OK ] Deleting existing %FILE% file.
@@ -64,3 +140,15 @@ if exist %FILE% (
 echo [ OK ] Now compiling...
 mkisofs -quiet -o "%FILE%" -p "%NAME%" -publisher "%PUBLISHER%" -V "%TITLE%" -b ISOLINUX/ISOLINUX.BIN -no-emul-boot -boot-load-size 4 -boot-info-table -N -J -r -c boot.catalog -hide boot.catalog -hide-joliet boot.catalog CDROOT
 echo [ OK ] Compile finished
+goto end
+
+:noarg
+echo You must specify a language as an argument. (EN, FR)
+goto end
+
+:errorarg
+echo "%1" isn't a correct argument.
+echo You must specify a language as an argument. (EN, FR)
+goto end
+
+:end
